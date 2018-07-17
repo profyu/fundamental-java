f8508a5722111f5c1a45af6c99c1f29c560be62f
profyu-edu <profyu.edu@gmail.com>
2018-07-17 22:08:46 +0800

    add comment

--- examples/rental-helper/src/main/java/com/example/retalhelper/App.java
+++ examples/rental-helper/src/main/java/com/example/retalhelper/App.java
@@ -16,22 +16,27 @@ public class App {
 	public static void main(String[] args) {
 
 		try {
-
+			// 信任所有SSL憑證發行單位
 			SSLContext sc = SSLContext.getInstance("SSL");
 			sc.init(null, new TrustManager[] { TrustManagerUtils.getAcceptAllTrustManager() }, new SecureRandom());
 			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
 
+			// 透過 ListPageCrawler 實例來抓回列表頁面當中，通往各屋詳細連頁面結網址
 			ListPageCrawler lpc = new ListPageCrawler(1, 1);
-
 			List<String> detailUrls = lpc.getDetailUrls();
 
+			// 用來存放所有爬回的租屋資料的清單
 			List<House> houses = new ArrayList<>();
 
+			// 走訪各詳細頁面
 			for (String detailUrl : detailUrls) {
 				System.out.println("詳細頁面網址: " + detailUrl);
 
+				// 透過 DetailPageCrawler 實例來抓取各屋詳細資料
 				DetailPageCrawler dpc = new DetailPageCrawler(detailUrl);
 				House house = dpc.getHouse();
+
+				// 如果成功才加入租屋資料清單
 				if (house != null) {
 					houses.add(house);
 					System.out.println(house.toString());

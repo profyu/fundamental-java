8f198c359a5cf6a6c65983ceb4231a16d9d2d6f4
profyu-edu <profyu.edu@gmail.com>
2018-08-01 15:35:11 +0800

    project daemon

--- examples/rental-helper/src/main/java/com/example/retalhelper/App.java
+++ examples/rental-helper/src/main/java/com/example/retalhelper/App.java
@@ -35,37 +35,49 @@ public class App {
 			MongoClient mongoClient = new MongoClient(uri);
 			Datastore datastore = morphia.createDatastore(mongoClient, "rental_helper");
 
-			// 透過 ListPageCrawler 實例來抓回列表頁面當中，通往各屋詳細連頁面結網址
-			ListPageCrawler lpc = new ListPageCrawler(1, 1);
-			List<String> detailUrls = lpc.getDetailUrls();
+			// 無限迴圈
+			while (true) {
+				// 抓取台北市 1~12 區
+				for (int sectionId = 1; sectionId <= 12; sectionId++) {
 
-			// 走訪各詳細頁面
-			for (String detailUrl : detailUrls) {
-				System.out.println("詳細頁面網址: " + detailUrl);
+					System.out.println("目前 sectionId = " + sectionId);
 
-				// 透過 DetailPageCrawler 實例來抓取各屋詳細資料
-				DetailPageCrawler dpc = new DetailPageCrawler(detailUrl);
-				House house = dpc.getHouse();
+					// 透過 ListPageCrawler 實例來抓回列表頁面當中，通往各屋詳細連頁面結網址
+					ListPageCrawler lpc = new ListPageCrawler(1, sectionId);
+					List<String> detailUrls = lpc.getDetailUrls();
 
-				// 如果成功才加入資料庫
-				if (house != null) {
+					// 走訪各詳細頁面
+					for (String detailUrl : detailUrls) {
+						System.out.println("詳細頁面網址: " + detailUrl);
 
-					// 查詢資料庫: 是否已經有符合的 postId 的租屋資料
-					List<House> existed = datastore.createQuery(House.class).field("postId").equal(house.getPostId())
-							.asList();
+						// 透過 DetailPageCrawler 實例來抓取各屋詳細資料
+						DetailPageCrawler dpc = new DetailPageCrawler(detailUrl);
+						House house = dpc.getHouse();
 
-					if (existed.size() == 0) {
-						datastore.save(house);
-						System.out.println("資料已寫入");
-					} else {
-						System.out.println("資料已存在");
+						// 如果成功才加入資料庫
+						if (house != null) {
+
+							// 查詢資料庫: 是否已經有符合的 postId 的租屋資料
+							List<House> existed = datastore.createQuery(House.class).field("postId")
+									.equal(house.getPostId()).asList();
+
+							if (existed.size() == 0) {
+								datastore.save(house);
+								System.out.println("資料已寫入");
+							} else {
+								System.out.println("資料已存在");
+							}
+							System.out.println(house.toString());
+						}
+
 					}
-					System.out.println(house.toString());
 				}
+				System.out.println("抓取完成");
+				Thread.sleep(300000);
 
 			}
 
-		} catch (IOException | NoSuchAlgorithmException | KeyManagementException e) {
+		} catch (IOException | NoSuchAlgorithmException | KeyManagementException | InterruptedException e) {
 			System.out.println("網頁抓取失敗");
 			e.printStackTrace();
 		}
